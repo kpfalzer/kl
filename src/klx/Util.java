f867c379df6b9d7a7a3909e893b20bd7b1fd752c
Admin User <adminuser@et-karl-mbp.local>
2019-04-03 06:57:58 -0700

    fix regex issue

--- src/klx/Util.java
+++ src/klx/Util.java
@@ -13,4 +13,20 @@ public class Util {
     public static String readFile(Path file) throws IOException {
         return new String(Files.readAllBytes(file));
     }
+
+    public static class Pair<T1, T2> {
+        public Pair() {
+            v1 = null;
+            v2 = null;
+        }
+
+        public Pair(T1 v1, T2 v2) {
+            this.v1 = v1;
+            this.v2 = v2;
+        }
+
+        public T1 v1;
+        public T2 v2;
+
+    }
 }
